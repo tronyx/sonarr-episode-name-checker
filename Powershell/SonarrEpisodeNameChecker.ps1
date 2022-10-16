828435f9100a26b87cad94246fb634d91ce3169a
Angry Cuban <39564898+angrycuban13@users.noreply.github.com>
2022-10-16 15:56:47 -0500

    Path Exclusion Addition & Script Cleanup

--- Powershell/SonarrEpisodeNameChecker.ps1
+++ Powershell/SonarrEpisodeNameChecker.ps1
@@ -7,23 +7,31 @@ param (
 
 #------------- DEFINE VARIABLES -------------#
 
-[string]$sonarrApiKey = ""
-[string]$sonarrUrl = ""
+[string]$sonarrApiKey = "9fece972e94d495a9c8a4fcc67b5d41f"
+[string]$sonarrUrl = "http://blackbox.lan:8989/sonarr"
 [string]$sonarrSeriesStatus = ""
 
 #------------- SCRIPT STARTS -------------#
 
 # Specify location of exclusions file, normally located one directory above the current script location.
 $seriesExclusionsFile = Join-Path (Get-Item $PSScriptRoot).Parent -ChildPath excludes\name_excludes.txt
+$seriesPathExclusionsFile = Join-Path (Get-Item $PSScriptRoot).Parent -ChildPath \excludes\path_excludes.txt
 
 # Import the contents of the series exclusion list.
-if (Test-Path $seriesExclusionsFile -PathType Leaf){
+if ((Test-Path $seriesExclusionsFile -PathType Leaf) -and (Test-Path $seriesPathExclusionsFile -PathType Leaf)){
     $seriesExclusions = Get-Content $seriesExclusionsFile
-    Write-Verbose "Exclusions loaded"
+    $seriesPathExclusions = Get-Content $seriesPathExclusionsFile
+    foreach ($seriesPathExclusion in $seriesPathExclusions){
+        if (-Not $seriesPathExclusion.EndsWith("/")){
+            throw "$($seriesPathExclusion) does not end in /"
+        }
+    }
+    Write-Output "Exclusions loaded"
+
 }
 
 else {
-    throw "Unable to locate exclusions file"
+    throw "Unable to locate exclusions files"
 }
 
 # Declare headers that will be passed on each API call.
@@ -38,29 +46,27 @@ if ($apiStatusCode -notmatch "2\d\d"){
     throw "Unable to retrieve Series Folder Format from Sonarr"
 }
 
-# Replaces characters based on your Series Folder Format from the exclusion list
-if ($namingConfig -eq '{Series TitleYear} {imdb-{ImdbId}}'){
-    $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\) \{imdb-tt(.*)\}", ""
-}
-
-# Replaces characters based on your Series Folder Format from the exclusion list
-elseif ($namingConfig -eq '{Series TitleYear} [imdb-{ImdbId}]'){
-    $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\) \[imdb-tt(.*)\]", ""
-}
-
-# Replaces characters based on your Series Folder Format from the exclusion list
-elseif ($namingConfig -eq '{Series TitleYear}'){
-    $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\)"
-}
-
-# I hate you Zak
-elseif ($namingconfig -eq '{Series TitleYear} [imdb-{ImdbId}][tvdb-{TvdbID}]'){
-    $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\) \[imdb-tt(.*)\]\[tvdb-(.*)\]",""
-}
-
-# If using a different naming scheme than what is recommended by TRaSH Guides, exit with error
-else {
-    throw "`nYou are not using a supported naming scheme. Supported naming schemes are:`n{Series TitleYear}`n{Series TitleYear} [imdb-{ImdbId}]`n{Series TitleYear} {imdb-{ImdbId}}"
+# Replaces illegal characters based on your Series Folder Format from the exclusion list
+switch ($namingConfig){
+    '{Series TitleYear} {imdb-{ImdbId}}' {
+        Write-Output "{Series TitleYear} {imdb-{ImdbId}} detected"
+        $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\) \{imdb-tt(.*)\}", ""
+    }
+    '{Series TitleYear} [imdb-{ImdbId}]'{
+        Write-Output "{Series TitleYear} [imdb-{ImdbId}] detected"
+        $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\) \[imdb-tt(.*)\]", ""
+    }
+    '{Series TitleYear}' {
+        Write-Output "{Series TitleYear} detected"
+        $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\)"
+    }
+    '{Series TitleYear} [imdb-{ImdbId}][tvdb-{TvdbID}]' {
+        Write-Output "{Series TitleYear} [imdb-{ImdbId}][tvdb-{TvdbID}] detected"
+        $seriesExclusions = $seriesExclusions -replace "\(\d\d\d\d\) \[imdb-tt(.*)\]\[tvdb-(.*)\]",""
+    }
+    Default {
+        throw "`nYou are not using a supported naming scheme. Supported naming schemes are:`n{Series TitleYear}`n{Series TitleYear} [imdb-{ImdbId}]`n{Series TitleYear} {imdb-{ImdbId}}"
+    }
 }
 
 # Retrieve all Sonarr series.
@@ -71,23 +77,23 @@ if ($apiStatusCode -notmatch "2\d\d"){
 }
 
 else {
-    Write-Verbose "Successfully loaded $($allSeries.count) series from Sonarr"
+    Write-Output "Successfully loaded $($allSeries.count) series from Sonarr"
 }
 
 # Filter out series with names that match anything in $seriesExclusions and anything that doesn't match the value of sonarrSeriesStatus
 if ($sonarrSeriesStatus -ne ""){
-    $filteredSeries = $allSeries | Where-Object {$_.title -notin $seriesExclusions -and $_.status -eq $($sonarrSeriesStatus)}
+    $filteredSeries = $allSeries | Where-Object {$_.title -notin $seriesExclusions -and $_.rootFolderPath -notin $seriesPathExclusions -and $_.status -eq $($sonarrSeriesStatus)}
 }
 
 # Filter out series with names that match anything in $seriesExclusions
 else {
-    $filteredSeries = $allSeries | Where-Object {$_.title -notin $seriesExclusions}
+    $filteredSeries = $allSeries | Where-Object {$_.title -notin $seriesExclusions -and $_.rootFolderPath -notin $seriesPathExclusions}
 }
 
-Write-Verbose "Series filtering completed, there are now $($filteredSeries.count) series left to process"
+Write-Output "Series filtering completed, there are now $($filteredSeries.count) series left to process"
 
 # Loop through each object in $filteredSeries.
-foreach ($series in $filteredSeries){
+<#foreach ($series in $filteredSeries){
 
     # Query the "rename" endpoint in Sonarr's API to determine if any episodes need to be renamed
     $episodesToRename = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/rename?seriesId=$($series.id)" -Headers $webHeaders -StatusCodeVariable apiStatusCode
@@ -98,7 +104,7 @@ foreach ($series in $filteredSeries){
     # If the rename endpoint has episodes to be renamed, proceed with refreshing series metadata
     if ($episodesToRename.count -gt 0){
 
-        Write-Verbose "Starting metadata refresh of $($series.Title)"
+        Write-Output "Starting metadata refresh of $($series.Title)"
 
         # Send command to Sonarr to refresh the series metadata in case the episode name has changed from what's cached
         $refreshSeries = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/command" -Headers $webHeaders -Method Post -ContentType "application/json" -Body "{`"name`":`"RefreshSeries`",`"seriesId`": $($series.id)}" -StatusCodeVariable apiStatusCode
@@ -112,7 +118,7 @@ foreach ($series in $filteredSeries){
             $tasks = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/command" -Headers $webHeaders -Method Get
             $refreshTask = $tasks | Where-Object {$_.id -eq $refreshSeries.id}
 
-            Write-Verbose "Waiting for metadata refresh for $($series.title) to be finished"
+            Write-Output "Waiting for metadata refresh for $($series.title) to be finished"
             Start-Sleep 5
         } until (
             $refreshTask.status -eq "completed"
@@ -120,7 +126,7 @@ foreach ($series in $filteredSeries){
 
         # If $renameSeries parameter is true, proceed with renaming files
         if ($renameSeries -eq $true){
-            Write-Verbose "Renaming episodes in $($series.title)"
+            Write-Output "Renaming episodes in $($series.title)"
 
             # If there's only one episode, modify API call to only send the episode
             if ($episodesToRename.count -eq 1){
@@ -141,7 +147,7 @@ foreach ($series in $filteredSeries){
                 $tasks = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/command" -Headers $webHeaders -Method Get
                 $renameTask = $tasks | Where-Object {$_.id -eq $renameFiles.id}
 
-                Write-Verbose "Waiting for files to be renamed for $($series.title)"
+                Write-Output "Waiting for files to be renamed for $($series.title)"
                 Start-Sleep 5
                 } until (
                     $renameTask.status -eq "completed"
@@ -157,16 +163,16 @@ foreach ($series in $filteredSeries){
         # Query Sonarr's API using the "$series.id" for a list of existing episodes
         $seriesEpisodes = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/episodefile?seriesid=$($series.id)" -Headers $webHeaders
 
-        Write-Verbose "$($series.title) has $($seriesepisodes.count) episodes"
+        Write-Output "$($series.title) has $($seriesepisodes.count) episodes"
 
         # Filter results from previous command to only include episodes with TBA (case sensitive) or Episode XXXX (case sensitive) in their file path.
         $episodesToRename = $seriesEpisodes | Where-Object {$_.relativepath -cmatch "TBA|Episode [0-9]{1,}"}
 
-        Write-Verbose "$($series.title) with ID $($series.id) has $($episodesToRename.count
+        Write-Output "$($series.title) with ID $($series.id) has $($episodesToRename.count
         ) episodes to be renamed"
 
         if ($episodesToRename.count -gt 0){
-            Write-Verbose "Starting metadata refresh of $($series.Title)"
+            Write-Output "Starting metadata refresh of $($series.Title)"
 
             # Send command to Sonarr to refresh the series metadata in case the episode name has changed from what's cached
             $refreshSeries = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/command" -Headers $webHeaders -Method Post -ContentType "application/json" -Body "{`"name`":`"RefreshSeries`",`"seriesId`": $($series.id)}" -StatusCodeVariable apiStatusCode
@@ -180,7 +186,7 @@ foreach ($series in $filteredSeries){
                 $tasks = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/command" -Headers $webHeaders -Method Get
                 $refreshTask = $tasks | Where-Object {$_.id -eq $refreshSeries.id}
 
-                Write-Verbose "Waiting for metadata refresh for $($series.title) to be finished"
+                Write-Output "Waiting for metadata refresh for $($series.title) to be finished"
                 Start-Sleep 5
             } until (
                 $refreshTask.status -eq "completed"
@@ -209,7 +215,7 @@ foreach ($series in $filteredSeries){
                     $tasks = Invoke-RestMethod -Uri "$($sonarrUrl)/api/v3/command" -Headers $webHeaders -Method Get
                     $renameTask = $tasks | Where-Object {$_.id -eq $renameFiles.id}
 
-                    Write-Verbose "Waiting for files to be renamed for $($series.title)"
+                    Write-Output "Waiting for files to be renamed for $($series.title)"
                     Start-Sleep 5
                     } until (
                         $renameTask.status -eq "completed"
@@ -220,4 +226,4 @@ foreach ($series in $filteredSeries){
             }
         }
     }
-}
+}#>
